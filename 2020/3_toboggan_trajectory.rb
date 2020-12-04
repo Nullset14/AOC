78e52eb7962f561b036daa3b3ccf92a2da3937ee
Ajay Kumar <Nullset14@users.noreply.github.com>
2020-12-04 01:20:16 -0800

    Create 3_toboggan_trajectory.rb

---
+++ 2020/3_toboggan_trajectory.rb
@@ -0,0 +1,21 @@
+def traverse(right, down, geology)
+  trees = 0
+  grid_x = 0
+  grid_y = 0
+
+  loop do
+    grid_x += down
+    grid_y = (grid_y + right).divmod(geology[0].length).last
+    trees += 1 if geology[grid_x][grid_y] == "#"
+
+    break if (grid_x + down >= geology.length)
+  end
+
+  trees
+end
+
+
+geology = input.split("\n").map(&:chars)
+
+puts traverse(3, 1, geology)
+puts [[3, 1], [1, 1], [5, 1], [7, 1], [1, 2]].inject(1) { |product, path| product * traverse(*path, geology) }
