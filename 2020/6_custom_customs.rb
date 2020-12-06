c932d0212a323e111ea6b7a5343df533d2d71d13
Ajay Kumar <Nullset14@users.noreply.github.com>
2020-12-05 23:42:54 -0800

    Update 6_custom_customs.rb

--- 2020/6_custom_customs.rb
+++ 2020/6_custom_customs.rb
@@ -1,11 +1,11 @@
 anyone_yes = 0
-everyone_sum = 0
+everyone_yes = 0
 
 input.split("\n\n").each do |group_response|
   person_responses = group_response.split("\n").map(&:chars)
   anyone_yes += person_responses.flatten.uniq.count
-  everyone_sum += person_responses.inject(:&).count
+  everyone_yes += person_responses.inject(:&).count
 end
 
 puts anyone_yes
-puts everyone_sum
+puts everyone_yes
